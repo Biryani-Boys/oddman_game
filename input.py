3aad7c99e52cb2a89685fb8c7414b426730c650f
Tejas Kannampuzha <43386445+Kannampuzha@users.noreply.github.com>
2021-08-08 21:38:30 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,82 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter.filedialog import askopenfilename
+
+from PIL import ImageTk, Image
+
+
+class App(Tk):
+
+    def __init__(self):
+        super().__init__()
+        self.canvas = Canvas(self, width=1000, height=500, bg='#92A8D1')
+        self.canvas.pack()
+
+        self.select_button = Button(self.canvas, text="Select", font=('calibri', 10), bd=3, width=20, height=2,
+                                    bg='#66ff33',
+                                    command=self.open, relief=RIDGE)
+        self.select_button.place(x=310, y=100)
+
+        self.canvas.create_rectangle(100, 170, 100 + 190, 170 + 150, outline='black',
+                                     fill="white")  # Make rectcangle for showing image
+
+        label = Label(self.canvas, text="Add File", bg="#92A8D1")
+        label.config(font=("Courier", 16))
+        label.place(x=100, y=100)
+
+        label2 = Label(self.canvas, text="Enter Tags", bg="#92A8D1")
+        label2.config(font=("Courier", 16))
+        label2.place(x=100, y=350)
+
+        self.inputtags = Text(self.canvas, height=1,
+                              width=50, font=("Courier", 16),
+                              bg="white")
+        self.inputtags.place(x=105, y=400)
+
+        self.finish_button = Button(self.canvas, height=2,
+                                    width=20,
+                                    text="Finish",
+                                    command=self.finish, bg="#66ff33")
+        self.finish_button.place(x=108, y=437)
+
+    def open(self):
+        self.file_chosen = askopenfilename(initialdir="/Desktop", title="Select file", filetypes=(
+            ('JPG', '*.jpg'), ('PNG', '*.png'), ('JPEG', '*jpeg')), defaultextension=".jpg")
+
+        try:
+
+            w = 189  # width of picture
+            h = 149  # height of picture
+
+            img = Image.open(self.file_chosen)  # Rendering the image
+            self.img = img.resize((w, h),
+                                  Image.ANTIALIAS)  # To fit the image in the box , we need to resize it , the modlue has inbuilt functions to do that.
+
+            self.image = ImageTk.PhotoImage(self.img)  # Loading a image object that can be used on canvas
+            self.canvas.create_image(101, 171, anchor=NW, image=self.image)
+
+
+
+        except:
+            messagebox.showinfo('Error')
+            return ()
+
+    def finish(self):
+        with open("count.txt", 'r') as count:
+            self.fileid = int(count.read()) + 1
+        with open("count.txt", 'w') as count:
+            count.write(str(self.fileid))
+
+        filename = f"Images/{self.fileid}.jpeg"  # The name of the new file
+
+        self.img.save(f"{filename}")  # saving the image file into the folder of the App
+
+        # File saving with the tags
+        with open("files.txt", 'a') as file:
+            file.write(f"{filename},{self.inputtags.get('1.0', 'end-1c')},\n")  # writing the data into the app
+
+        exit()
+
+
+if __name__ == '__main__':
+    App().mainloop()
---
+++ main.py
@@ -0,0 +1,150 @@
+from random import randint
+from tkinter import *
+
+from PIL import ImageTk, Image
+
+from selector import selector_session
+
+
+class App(Tk):
+
+    def __init__(self):
+        super().__init__()
+
+        self.correct = None
+        self.options = {'A': None, 'B': None, 'C': None, 'D': None}
+        self.canvas = Canvas(self, width=1000, height=500, bg='#92A8D1')
+        self.canvas.pack()
+
+        self.w = 190
+        self.h = 150
+        self.score = 0
+
+        l = Label(self.canvas, text="WHICH IS THE ODD MAN ?", bg="#92A8D1")
+        l.config(font=("Courier", 16))
+        l.place(x=100, y=100)
+
+        A = Label(self.canvas, text="A", bg="#92A8D1")
+        A.config(font=("Courier", 16))
+        A.place(x=183, y=332)
+
+        B = Label(self.canvas, text="B", bg="#92A8D1")
+        B.config(font=("Courier", 16))
+        B.place(x=392, y=333)
+
+        C = Label(self.canvas, text="C", bg="#92A8D1")
+        C.config(font=("Courier", 16))
+        C.place(x=589, y=332)
+
+        D = Label(self.canvas, text="D", bg="#92A8D1")
+        D.config(font=("Courier", 16))
+        D.place(x=794, y=336)
+
+        self.scorebutton = Label(self.canvas, text=f"score:{self.score}", bg="#92A8D1")
+        self.scorebutton.config(font=("Courier", 16))
+        self.scorebutton.place(x=651, y=419)
+
+        self.canvas.create_rectangle(100, 170, 100 + self.w, 170 + self.h, outline='#FF6F61')
+        self.canvas.create_rectangle(300, 170, 300 + self.w, 170 + self.h, outline='#FF6F61')
+        self.canvas.create_rectangle(500, 170, 500 + self.w, 170 + self.h, outline='#FF6F61')
+        self.canvas.create_rectangle(700, 170, 700 + self.w, 170 + self.h, outline='#FF6F61')
+
+        self.inputtxt = Text(self.canvas, height=1,
+                             width=3, font=("Courier", 16),
+                             bg="white")
+        self.inputtxt.place(x=475, y=350)
+        self.Output = Label(self.canvas, text="   ")
+        self.Output.config(font=("Courier", 16))
+
+        self.Output.place(x=111, y=416)
+
+        self.Display = Button(self.canvas, height=2,
+                              width=20,
+                              text="Check",
+                              command=lambda: self.Take_input(), bg="#66ff33")
+        self.Display.place(x=111, y=415 - 50)
+
+        self.Pass_Button = Button(self.canvas, height=2, width=20, text="PASS", bg="#66ff33",
+                                  command=self.display_images)
+        self.Pass_Button.place(x=713, y=90)
+
+    def Take_input(self):
+        INPUT = self.inputtxt.get("1.0", "end-1c")
+        print(INPUT)
+        if (INPUT == self.correct):
+            self.Output.destroy()
+            self.Output = Label(self.canvas, text="Correct")
+            self.Output.config(font=("Courier", 16))
+            self.Output.place(x=111, y=416)
+
+            # Restart the stuff
+            self.changescore()
+
+            self.display_images()
+
+
+
+
+        else:
+            self.Output.destroy()
+            self.Output = Label(self.canvas, text="Wrong")
+            self.Output.config(font=("Courier", 16))
+            self.Output.place(x=111, y=416)
+
+    def changescore(self):
+        self.score = self.score + 1
+
+        self.scorebutton.destroy()
+        self.scorebutton = Label(self.canvas, text=f"score:{self.score}")
+        self.scorebutton.config(font=("Courier", 16))
+        self.scorebutton.place(x=651, y=419)
+
+    def display_images(self):
+        try:
+            images_to_load = list(selector_session())
+
+            correct_option = images_to_load[3]
+
+            # Distributing the images into A,B,C,D randomly and also getting the correct option marked.
+            for x, y in zip((3, 2, 1, 0), ("D", "C", "B", "A")):
+                chosen = images_to_load.pop(randint(0, x))
+                if chosen == correct_option:
+                    self.correct = y
+                self.options[y] = chosen
+
+            # Correct answer
+            # print(self.correct)
+
+            # Loading the image and putting in rectangle.
+
+            self.imgA = Image.open(self.options['A'])  # Rendering the image
+            self.imgA = self.imgA.resize((self.w - 1, self.h - 1),
+                                         Image.ANTIALIAS)  # To fit the image in the box , we need to resize it , the modlue has inbuilt functions to do that.
+            self.imageA = ImageTk.PhotoImage(self.imgA)  # Loading a image object that can be used on canvas
+            A = self.canvas.create_image(101, 171, anchor=NW, image=self.imageA)
+
+            self.imgB = Image.open(self.options['B'])  # Rendering the image
+            self.imgB = self.imgB.resize((self.w - 1, self.h - 1),
+                                         Image.ANTIALIAS)  # To fit the image in the box , we need to resize it , the modlue has inbuilt functions to do that.
+            self.imageB = ImageTk.PhotoImage(self.imgB)  # Loading a image object that can be used on canvas
+            B = self.canvas.create_image(301, 171, anchor=NW, image=self.imageB)
+
+            self.imgC = Image.open(self.options['C'])  # Rendering the image
+            self.imgC = self.imgC.resize((self.w - 1, self.h - 1),
+                                         Image.ANTIALIAS)  # To fit the image in the box , we need to resize it , the modlue has inbuilt functions to do that.
+            self.imageC = ImageTk.PhotoImage(self.imgC)  # Loading a image object that can be used on canvas
+            C = self.canvas.create_image(501, 171, anchor=NW, image=self.imageC)
+
+            self.imgD = Image.open(self.options['D'])  # Rendering the image
+            self.imgD = self.imgD.resize((self.w - 1, self.h - 1),
+                                         Image.ANTIALIAS)  # To fit the image in the box , we need to resize it , the modlue has inbuilt functions to do that.
+            self.imageD = ImageTk.PhotoImage(self.imgD)  # Loading a image object that can be used on canvas
+            D = self.canvas.create_image(701, 171, anchor=NW, image=self.imageD)
+        except:
+            self.display_images()
+
+if __name__ == '__main__':
+    Game = App()
+    Game.display_images()
+    Game.mainloop()
+
